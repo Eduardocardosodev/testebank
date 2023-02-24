955ef13a93488be9dc18bacce5282064372e501d
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2023-02-24 12:15:04 -0300

    add

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,23 @@
-import { Router } from 'express';
-import { UserController } from './controllers/UserController';
-import { AuthMiddlewares } from './middlewares/auth';
-import { sendEmail } from './middlewares/sendEmail';
-import { userFormValidator } from './middlewares/validation';
+import { Router } from "express";
+import { UserController } from "./controllers/UserController";
+import { AuthMiddlewares } from "./middlewares/auth";
+import { sendEmail } from "./middlewares/sendEmail";
+import { userFormValidator } from "./middlewares/validation";
 
 const routes = Router();
 
 const user = new UserController();
 
-routes.post('/register', userFormValidator, sendEmail, user.create);
-routes.post('/login', AuthMiddlewares, user.login);
-routes.get('/getAllUser', user.index);
-routes.get('/getAllUser/:id', user.getUserById);
-routes.delete('/deleteUser/:id', user.delete);
-routes.get('/search', user.search);
+routes.post(
+  "https://testebank.onrender.com/register",
+  userFormValidator,
+  sendEmail,
+  user.create
+);
+routes.post("/login", AuthMiddlewares, user.login);
+routes.get("/getAllUser", user.index);
+routes.get("/getAllUser/:id", user.getUserById);
+routes.delete("/deleteUser/:id", user.delete);
+routes.get("/search", user.search);
 
 export default routes;
